3844dd7d5cd750d3d898dff1375faf5108e144ba
congcong1993 <18829291885@163.com>
2016-08-04 23:06:46 +0800

    add demo5 of superagent to github

---
+++ client/demo5.js
@@ -0,0 +1,13 @@
+let req = require('superagent');
+req.post('localhost:3000/json')
+    .type('form')
+    .set('Content-Type','application/json')
+    .send({"name":"fcc"})
+    .end(function (err,res) {
+        if(res.ok){
+            console.log("ok," +res.text)
+        }
+        else {
+            console.log("error")
+        }
+    });
