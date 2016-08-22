10e8109caf058e21c648a7c6ba3d9a418f57155d
fcc1885 <18829291885@163.com>
2016-08-22 18:06:21 +0800

    1

--- client/demo1.js
+++ client/demo1.js
@@ -6,7 +6,8 @@ req
     .end(function (err, res) {
         if (res.ok) {
             console.log("right," + res.text);
-        } else {
+        
+} else {
             console.log("error");
         }
     });
