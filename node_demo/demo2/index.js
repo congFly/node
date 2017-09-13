6c5ae1aa8964cfed304d9072b120be70bc79be45
fcc <fcc@cognichain.com>
2017-09-13 15:54:03 +0800

    在index.js中启动服务

---
+++ node_demo/demo2/index.js
@@ -0,0 +1,6 @@
+/**
+ * Created by 18829 on 2017/9/13.
+ */
+var server = require('./server');
+
+server.start();
--- node_demo/demo2/server.js
+++ node_demo/demo2/server.js
@@ -10,14 +10,19 @@ var http = require("http");
  response.end();
  }).listen(8888);*/
 
-function onRequest(request, response) {
-    console.log('request received!');
-    response.writeHead(200, {"content-Type": "text-plain"});
-    response.write("fcc");
-    response.end();
+
+function start() {
+    function onRequest(request, response) {
+        console.log("Request received.");
+        response.writeHead(200, {"Content-Type": "text/plain"});
+        response.write("Hello World");
+        response.end();
+    }
+
+    http.createServer(onRequest).listen(8888);
+    console.log("Server has started.");
 }
 
+exports.start = start;
 
-http.createServer(onRequest).listen(8888);
 
-console.log('server has start');
